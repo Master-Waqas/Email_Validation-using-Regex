d5060f31dc2c8fd0d162e3af1a27ea86d61f842f
Master-Waqas <168361233+Master-Waqas@users.noreply.github.com>
2024-05-05 09:08:33 -0700

    Add files via upload
    This Email Validator using regex includes:
    
    1. All letters Lowercase or Uppercase.
    2. Digits 0 - 9
    3. . _time 1
    4. @ time 1
    5. . 2, .3 position at end

---
+++ Email_Validation using Regex.py
@@ -0,0 +1,9 @@
+import re
+email_condition = "^[a-zA-z]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
+user_email = input("Enter Email : ")
+
+if re.search(email_condition, user_email):
+    print("Right Email.")
+
+else:
+    print("Wrong Email.")
